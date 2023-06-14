1f29d2601e2ff4903209ed3a4caa9270f21deead
Juan Negrete <negrete.juan85@gmail.com>
2023-06-13 22:33:58 -0600

    Install adminlte

--- routes/web.php
+++ routes/web.php
@@ -16,3 +16,8 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+Route::post('login', [ 'as' => 'login', 'uses' => 'LoginController@do']);
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
